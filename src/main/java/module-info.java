6b29b2df47998fff3cb41ed1353474caf522629a
belkamydog <120400903+belkamydog@users.noreply.github.com>
2025-01-17 20:24:32 +0300

    start project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.typing.fasttyping {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.typing.fasttyping to javafx.fxml;
+    exports com.typing.fasttyping;
+}
